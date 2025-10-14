89fb506c05168d7daef5adef8e29a16ed43c216c
primoboosttt <primoboostai@gmail.com>
2025-10-14 12:28:01 +0530

    Updated JobsPage.tsx

--- src/components/pages/JobsPage.tsx
+++ src/components/pages/JobsPage.tsx
@@ -241,7 +241,7 @@ export const JobsPage: React.FC<JobsPageProps> = ({
         {/* Jobs Grid */}
         {!error && (
           <>
-            <div className="grid grid-cols-1 gap-6 mb-8">
+           <div className="container mx-auto px-2 sm:px-4 py-8">
               {jobs.map((job, index) => (
                 <JobCard
                   key={job.id}
